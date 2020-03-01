3a967ad2d1ff3c4684404a80e4ed439c9544bff9
Leonardo Henrique <leohenricardoso@gmail.com>
2020-03-01 01:40:06 -0300

    :construction: CRUD completo twitter

---
+++ start/routes.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use('Route')
+
+Route.post('/register', 'AuthController.register')
+Route.post('/authenticate', 'AuthController.authenticate')
+Route.get('/app', 'AppController.index').middleware(['auth'])
+
+Route.group(() => {
+  Route.resource('tweets', 'TweetController').apiOnly()
+}).middleware('auth')
